512dff37bde1279888a01f50ab1703752ad3b9a3
Jeffrey <jeffreyrichardlin@gmail.com>
2016-12-15 17:12:44 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pybuilder-nose",
+    packages=['pybuilder-nose'],
+    version="0.0.5",
+    description="A call stack profiler for Python. Inspired by Apple's Instruments.app",
+    author='Alex Dow',
+    url='https://github.com/jljs/pybuilder_nose',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Testing',
+    ]
+)
